e79b58dc352661aab63bbd140ffc00888f2fc1e4
gustavtjac <gustavtorup@gmail.com>
2026-03-04 15:08:22 +0100

    setup base express.js project + serving frontpage.html

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict"
+//setup Express.js libary
+import express from 'express';
+const app = express();
+//serve public folder as static 
+app.use(express.static('public'));
+//Use path instead of __dirname
+import path from 'path';
+
+//_________________________ENDPOINTS_______________________________________
+//____________________________GET__________________________________________
+//Serve frontend
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/frontpage/frontpage.html"))
+});
+
+//____________________________POST__________________________________________
+
+
+//_________________________PUT/PATCH________________________________________
+
+
+//___________________________DELETE_________________________________________
+
+
+
+//___________________________LISTEN_________________________________________
+app.listen(8080, () => {
+    console.log("Server running on port 8080")
+});
+
+
