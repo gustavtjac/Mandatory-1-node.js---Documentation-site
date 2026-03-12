fc91b9b16077e9ce3795d12a1c6e33f6149b14b5
gustavtjac <gustavtorup@gmail.com>
2026-03-12 22:33:10 +0100

    leg med paths for at få vercel til at virke

--- util/templateEngine.js
+++ util/templateEngine.js
@@ -1,8 +1,16 @@
 import fs from 'fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { createAllSideBarNoteOptions } from './sideBarUtil.js';
-import { getAllNotes, getNote} from './notesUtil.js';
+import { getAllNotes} from './notesUtil.js';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const projectRoot = path.resolve(__dirname, '../');
 
+export function readPage(filePath) {
+    const absolutePath = path.resolve(projectRoot, filePath);
+    return fs.readFileSync(absolutePath, 'UTF-8').toString();
+}
 
 export function constructPage(page, note){
 
@@ -19,6 +27,3 @@ export function constructPage(page, note){
 }
 
 
-export function readPage(path) {
-   return fs.readFileSync(path,'UTF-8').toString();
-}
